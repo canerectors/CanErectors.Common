89c4fad490021b10c14b9f5afe71c6a1b7c35307
James Fera <james.fera@gmail.com>
2017-05-31 09:45:59 -0400

    pretty type names

---
+++ CanErectors.Common.Tests/TypeExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CanErectors.Common.Tests
+{
+    public class TypeExtensionTests
+    {
+        [Fact]
+        public void GenericTypeShouldNameShouldBePretty()
+        {
+            var genericType = typeof(IEnumerable<>).MakeGenericType(typeof(string));
+
+            var prettyName = genericType.PrettyTypeName();
+
+            Assert.Equal(prettyName, "System.Collections.Generic.IEnumerable<System.String>");
+        }
+    }
+}
---
+++ CanErectors.Common/Extensions/TypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanErectors.Common
+{
+    public static class TypeExtensions
+    {
+        public static string PrettyTypeName(this Type t)
+        {
+            if (t.IsGenericType)
+                return $"{t.FullName.Substring(0, t.FullName.LastIndexOf("`", StringComparison.InvariantCulture))}<{string.Join(", ", t.GetGenericArguments().Select(PrettyTypeName))}>";
+
+            return t.FullName;
+        }
+    }
+}
